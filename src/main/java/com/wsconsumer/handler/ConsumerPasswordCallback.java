64b1f7ed647d6e746f20a925313e3f9fee4478ef
Mohit Arun Inampudi <mi9.developer@gmail.com>
2017-10-15 20:05:11 -0400

    added authorization

---
+++ src/main/java/com/wsconsumer/handler/ConsumerPasswordCallback.java
@@ -0,0 +1,31 @@
+package com.wsconsumer.handler;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+public class ConsumerPasswordCallback implements CallbackHandler {
+
+	Map<String, String> credentials = new HashMap<>();
+
+	public ConsumerPasswordCallback() {
+		credentials.put("admin", "password");
+	}
+
+	@Override
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+		for (Callback callback : callbacks) {
+			WSPasswordCallback wspc = (WSPasswordCallback) callback;
+			String string = credentials.get(wspc.getIdentifier());
+			wspc.setPassword(string);
+			return;
+		}
+	}
+
+}
--- src/main/java/com/wsconsumer/test/SumServiceTest.java
+++ src/main/java/com/wsconsumer/test/SumServiceTest.java
@@ -5,9 +5,17 @@ import static org.junit.Assert.assertNotNull;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
+import org.apache.wss4j.dom.WSConstants;
+import org.apache.wss4j.dom.handler.WSHandlerConstants;
 import org.junit.Test;
 
+import com.wsconsumer.handler.ConsumerPasswordCallback;
 import com.wsproducer.service.Sum;
 import com.wsproducer.service.SumRequest;
 import com.wsproducer.service.SumResponse;
@@ -20,6 +28,20 @@ public class SumServiceTest {
 		try {
 			SumService sumService = new SumService(new URL("http://localhost:8080/webservices-security-wsproducer/services/sumService?wsdl"));
 			Sum sumPort = sumService.getSumPort();
+			
+			Client client = ClientProxy.getClient(sumPort);
+			Endpoint endpoint = client.getEndpoint();
+			
+			HashMap<String, Object> outDetails = new HashMap<>();
+			outDetails.put(WSHandlerConstants.ACTION, "UsernameToken");
+			outDetails.put(WSHandlerConstants.USER, "admin");
+			outDetails.put(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+			outDetails.put(WSHandlerConstants.PW_CALLBACK_CLASS, ConsumerPasswordCallback.class.getName());
+			
+			WSS4JOutInterceptor wss4jOutInterceptor = new WSS4JOutInterceptor(outDetails);
+			
+			endpoint.getOutInterceptors().add(wss4jOutInterceptor);
+			
 			SumRequest sumRequest = new SumRequest();
 			sumRequest.setNum1(25);
 			sumRequest.setNum2(25);
